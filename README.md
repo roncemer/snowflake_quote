500c519926214e0caded643f3faad7ba1d80112c
Ron Cemer <ron@roncemer.com>
2022-12-06 09:50:03 -0600

    Add initial code.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # snowflake_quote
 A Python3 function to properly escape and quote string literals for Snowflake
+
+Copyright (c) 2022 Ronald B. Cemer
+Licensed under the MIT license.
+
+# MANDATORY USAGE
+
+If you are quoting string literals in Python3 for the purpose of building SQL statements for Snowflake, you *MUST* use this library, or a functionally identical equivalent, in order to avoid creating SQL injection vectors which can be easily exploited by malicious actors.
+
+# Running the tests
+
+To run the tests:
+
+```sh
+python3 src/test/test_snowflake_quote.py
+```
---
+++ src/main/snowflake_quote.py
@@ -0,0 +1,29 @@
+# snowflake_quote
+# A Python3 function to properly escape and quote string literals for Snowflake
+#
+# Copyright (c) 2022 Ronald B. Cemer
+# Licensed under the MIT license.
+
+def snowflake_quote(s):
+  result = "'"
+  for c in s:
+    if (c == "'" or c == '"' or c == '\\'):
+      result = result + '\\' + c
+    elif (c == '\b'):
+      result = result + '\\b'
+    elif (c == '\f'):
+      result = result + '\\f'
+    elif (c == '\n'):
+      result = result + '\\n'
+    elif (c == '\r'):
+      result = result + '\\r'
+    elif (c == '\t'):
+      result = result + '\\t'
+    else:
+      cc = ord(c)
+      if ((cc >= 0x00 and cc <= 0x1f) or cc >= 0x7f):
+        result = result + ("\\x%0.2x" % cc)
+      else:
+        result = result + c
+  result = result + "'"
+  return result
